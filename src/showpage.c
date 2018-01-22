ed97d4480f194830ea485d75d370ba559e7e3427
B4ckslash <moral1tycor3@gmail.com>
2018-01-22 11:15:30 +0100

    Added basic XML reading functionality

--- src/showpage.c
+++ src/showpage.c
@@ -1,12 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <libxml/xmlreader.h>
 #include "ShowPageConf.h"
+#include <unistd.h>
+#include <string.h>
+
+typedef struct pagelist *ptr;
+struct pagelist{
+                ptr prev;
+                int ID;
+                ptr next;
+                } start;
+
+
+void openPage(xmlTextReaderPtr reader)
+{
+    const unsigned char *content = xmlTextReaderConstValue(reader);
+    if(xmlTextReaderHasValue(reader)){
+        fprintf(stdout, "Read %s\n", content);
+    }
+}
+
 
 int main(int argc, char *argv[])
 {
-    fprintf(stdout, "%s Version %d.%d\n",
+    if(argc != 2){
+        fprintf(stdout, "Usage: %s (start | stop | restart)\n", argv[0]);
+        return 0;
+    }
+    xmlTextReaderPtr reader;
+    int ret;
+
+    reader = xmlNewTextReaderFilename("/home/marcus/pages.xml");
+
+    if (reader == NULL) {
+        fprintf(stderr, "Cannot open document \n");
+        return 1;
+    }
+    ret = xmlTextReaderRead(reader);
+    while(ret == 1){
+        openPage(reader);
+        ret = xmlTextReaderRead(reader);
+    }
+
+
+    /*    fprintf(stdout, "%s Version %d.%d\n",
             argv[0],
             ShowPage_VERSION_MAJOR,
             ShowPage_VERSION_MINOR);
-    return 0;
+    return 0;*/
 }
