11fcbb90fb8695d8f10cf83f882ad6b93ab41b25
Marcus Köhler <khler.marcus@gmail.com>
2018-01-24 08:00:43 +0100

    did more work on pagecycler

--- src/pagecycle.c
+++ src/pagecycle.c
@@ -10,9 +10,86 @@ typedef struct pagelist{
                 int ID;
                 ptr next;
                 } List;
+int running;
+
+ptr init_plist(char *arg)
+{
+    int arr_size = 0;
+    char *id;
+    /*calling strtok twice, once to get the necessary array size and once to populate the array*/
+    id = strtok(arg, ",");
+    while(id != NULL){
+        arr_size++;
+        id = strtok(NULL, ",");
+    }
+
+    if(arr_size == 0)
+        return NULL;
+
+    int ids[arr_size];
+    memset(ids, 0, arr_size*sizeof(int));
+    id = strtok(arg, ",");
+    int i = 0;
+    while(id != NULL){
+        ids[i] = atoi(id);
+        i++;
+        id = strtok(NULL, ",");
+    }
+
+    List *start = malloc(sizeof(List));
+    i = 0;
+    start->ID = ids[0];
+    List *cur = start;
+    while(i <= arr_size){
+        if(i < arr_size){
+            cur->next = malloc(sizeof(List));
+            cur->next->prev = cur;
+            cur = cur->next;
+            cur->ID = ids[i];
+        }else{
+            cur->next = start;
+            start ->prev = cur;
+        }
+        i++;
+    }
+    return start;
+}
+
+void free_list(ptr list)
+{
+    list->prev->next = NULL;
+    while(list != NULL)
+    {
+        ptr tmp = list;
+        list = tmp->next;
+        free(tmp);
+    }
+}
+
+void cycle(ptr node)
+{
+    FILE *fp = popen("xargs chromix-too focus", "w");
+    while(running){
+        fprintf(fp, "%d", node->ID);
+        fprintf(stdout, "Focussing %d\n", node->ID);
+        sleep(30);
+    }
+    pclose(fp);
+}
+
+void term_handle(int sig)
+{
+    running = 0;
+}
 
 int main(int argc, char *argv[])
 {
+    if(signal(SIGTERM, term_handle) < 0)
+    {
+        perror("Could not register SIGTERM handler");
+        return 1;
+    }
+    ptr strtPtr;
     if(argc != 2 && argc != 4){
         fprintf(stdout, "Usage: %s [-o old Tab IDs] new Tab IDs (Tab IDs are to be formatted as a \
             comma separated string)\n", argv[0]);
@@ -35,6 +112,19 @@ int main(int argc, char *argv[])
             id = strtok(NULL, ",");
         }
         pclose(pipe);
+        strtPtr = init_plist(argv[3]);
+    }else{
+        strtPtr = init_plist(argv[2]);
     }
-   return 0; 
+    
+    if(strtPtr == NULL){
+        fprintf(stderr, "Could not initialize page list");
+        return 1;
+    }
+
+    running = 1;
+    cycle(strtPtr);
+    free_list(strtPtr);
+
+   return 0;
 }
--- src/showpage.c
+++ src/showpage.c
@@ -18,11 +18,106 @@ void openPage(const xmlTextReaderPtr reader, FILE *fp)
     }
 }
 
+int read_xml()
+{
+    xmlTextReaderPtr reader;
+    int ret;
+
+    reader = xmlNewTextReaderFilename("/home/marcus/pages.xml");
+
+    if (reader == NULL) {
+        fprintf(stderr, "Cannot open document \n");
+        return 1;
+    }
+    ret = xmlTextReaderRead(reader);
+    FILE *fp = popen("xargs chromix-too open \0", "w\0");
+    while(ret == 1){
+        openPage(reader, fp);
+        ret = xmlTextReaderRead(reader);
+    }
+    pclose(fp);
+    return 0;
+}
+
 void signal_handler(int sig)
 {
     recvd_sig = sig;
 }
 
+
+pid_t fork_cserver(int pipe[])
+{
+    pid_t pid_cserver;
+    if((pid_cserver = fork()) < 0){
+        perror("Could not fork process for chromix server");
+        return pid_cserver;
+    }else if(pid_cserver == 0){
+        /*
+         * Execution branch of child process for chromix-too-server
+         *
+         * The pipe declared earlier is used to redirect stdout of the child process
+         * This is so that the opening of URLs can be held off until chromix has
+         * established a connection with chromium.
+         */
+        fprintf(stdout, "Starting chromix-too-server with PID %d\n", getpid());
+        fprintf(stdout, "Redirecting stdout of process %d to pipe to process %d\n", getpid(), getppid());
+
+        /*close read end of pipe*/
+        if(close(pipe[0]) < 0){
+            perror("Could not close read end of pipe");
+            return -1;
+        }
+        /*actually redirect stdout to pipe*/
+        if(dup2(pipe[1], STDOUT_FILENO) < 0){
+            perror("Could not redirect stdout to pipe");
+            return -1;
+        }
+
+        char *cserver_path ="/usr/local/bin/chromix-too-server";
+        char *const argv_cserver[] = {cserver_path, NULL};
+        execv(cserver_path, argv_cserver);
+    }
+
+    return pid_cserver;
+}
+
+pid_t fork_chrm()
+{
+    pid_t pid_chrm;
+    if((pid_chrm = fork()) < 0){
+        perror("Could not fork process for chrome");
+        return pid_chrm;
+    }else if(pid_chrm == 0){
+        fprintf(stdout, "Forking process for chrome with PID %d\n", getpid());
+        char *chrm_path="/usr/bin/chromium-browser";
+        char *const argv_chrm[] = {chrm_path, "--kiosk\0", NULL};
+        execv(chrm_path, argv_chrm);
+    }
+    return pid_chrm;
+}
+
+pid_t fork_pagec()
+{
+    pid_t pid;
+    FILE *fp = popen("chromix-too tid", "r");
+    char IDs[50];
+    char buf[2];
+    while(fgets(buf, sizeof(buf), fp) != NULL){
+        strcat(IDs, buf);
+        strcat(IDs, ",");
+        memset(buf, '\0', sizeof(buf));
+    }
+    if((pid = fork()) < 0){
+        perror("Could not fork process for pagecycler");
+        return 1;
+    }else if(pid == 0){
+        fprintf(stdout, "Forked process for pagecycler with PID %d and IDs %s\n", getpid(), IDs);
+        char *path = "/home/marcus/git/showpage_c/build/pagecycle";
+        char *const argv_pagec[] = {path, IDs, NULL};
+        execvp(path, argv_pagec);
+    }
+    return pid;
+}
 
 int main(int argc, char *argv[])
 {
@@ -37,36 +132,9 @@ int main(int argc, char *argv[])
     int pipefd[2];
     pipe(pipefd);
 
-    /*Fork and exec calls*/
-    if((pid_cserver = fork()) < 0){
-        perror("Could not fork process for chromix server");
-        goto term_routine;
-    }else if(pid_cserver == 0){
-        /*
-         * Execution branch of child process for chromix-too-server
-         *
-         * The pipe declared earlier is used to redirect stdout of the child process
-         * This is so that the opening of URLs can be held off until chromix has
-         * established a connection with chromium.
-         */
-        fprintf(stdout, "Starting chromix-too-server with PID %d\n", getpid());
-        fprintf(stdout, "Redirecting stdout of process %d to pipe to process %d\n", getpid(), getppid());
- 
-        /*close read end of pipe*/
-        if(close(pipefd[0]) < 0){
-            perror("Could not close read end of pipe");
+    pid_cserver = fork_cserver(pipefd);
+    if(pid_cserver < 0)
             goto term_routine;
-        }
-        /*actually redirect stdout to pipe*/
-        if(dup2(pipefd[1], STDOUT_FILENO) < 0){
-            perror("Could not redirect stdout to pipe");
-            goto term_routine;
-        }
-
-        char *cserver_path ="/usr/local/bin/chromix-too-server";
-        char *const argv_cserver[] = {cserver_path, NULL};
-        execv(cserver_path, argv_cserver);
-    }
 
     /*immediately close fd for the write end of the pipe if parent*/
     if(close(pipefd[1]) < 0){
@@ -74,15 +142,9 @@ int main(int argc, char *argv[])
         goto term_routine;
     }
 
-    if((pid_chrm = fork()) < 0){
-        perror("Could not fork process for chrome");
-        goto term_routine;
-    }else if(pid_chrm == 0){
-        fprintf(stdout, "Forking process for chrome with PID %d\n", getpid());
-        char *chrm_path="/usr/bin/chromium-browser";
-        char *const argv_chrm[] = {chrm_path, "--kiosk\0", NULL};
-        execv(chrm_path, argv_chrm);
-    }
+    pid_chrm = fork_chrm();
+    if(pid_chrm < 0)
+            goto term_routine;
 
     /* TODO: Add pagecycling functionality and spawn the cycler as a child process(preferably after pages have been opened)*/
 
@@ -102,23 +164,12 @@ int main(int argc, char *argv[])
         }
     }
 
-    xmlTextReaderPtr reader;
-    int ret;
+    if(read_xml())
+            goto term_routine;
 
-    reader = xmlNewTextReaderFilename("/home/marcus/pages.xml");
-
-    if (reader == NULL) {
-        fprintf(stderr, "Cannot open document \n");
+    pid_pagec = fork_pagec();
+    if(pid_pagec < 0)
         goto term_routine;
-        return 1;
-    }
-    ret = xmlTextReaderRead(reader);
-    FILE *fp = popen("xargs chromix-too open \0", "w\0");
-    while(ret == 1){
-        openPage(reader, fp);
-        ret = xmlTextReaderRead(reader);
-    }
-    pclose(fp);
 
     while(!recvd_sig){
         sleep(1);
@@ -127,6 +178,7 @@ int main(int argc, char *argv[])
 term_routine:
         kill(pid_chrm, SIGTERM);
         kill(pid_cserver, SIGTERM);
+        kill(pid_pagec, SIGTERM);
 
     return 0;
 }
